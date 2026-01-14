8c1f79c92fe7c306cfe3418045694df3241de66d
Avery Uang <avery.uang@crpusd.org>
2026-01-14 09:13:00 -0800

    Fresh Start

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -7,54 +7,39 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command_ping():
+def do_command():
     subprocess.call("ping localhost")
-
-def do_command_ipconfig():
-    subprocess.call("ipconfig")
-
-def do_command_Netstat():
-    subprocess.call("netstat")
-
-def do_command_tracert():
-    subprocess.call("tracert localhost")
-
-def do_command_nslookup():
-    subprocess.call("nslookup localhost")
-
-def do_command_nmap():
-    subprocess.call("nmap localhost")
 
 root = tk.Tk()
 frame = tk.Frame(root)
-frame.configure(width =300, height=200)
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command_ping)
+ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
 
-ipconfig_btn = tk.Button(frame, text="ipconfig", command=do_command_ipconfig)
-ipconfig_btn.pack()
-
-netstat_btn = tk.Button(frame, text="netstat", command=do_command_Netstat)
-netstat_btn.pack()
-
-tracert_btn = tk.Button(frame, text="tracert", command=do_command_tracert)
-tracert_btn.pack()
-
-nslookup_btn = tk.Button(frame, text="nslookup", command=do_command_nslookup)
-nslookup_btn.pack()
-
-nmap_btn = tk.Button(frame, text="nmap", command=do_command_nmap)
-nmap_btn.pack()
-
-listbox = tk.Listbox(frame)
-listbox.insert(1, ping_btn)
-listbox.insert(2, ipconfig_btn)
-listbox.insert(3, netstat_btn)
-listbox.insert(4, tracert_btn)
-listbox.insert(5, nslookup_btn)
-listbox.insert(6, nmap_btn)
-listbox.pack()
 root.mainloop()
+
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
+
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()

5aaea6f0b60aed252ff18338e9c04056501a1bab
Josh Pine <joshua.pine@crpusd.org>
2026-01-14 10:03:26 -0800

    End

--- p227_starter_one_button_shell.py
+++ p227_starter_one_button_shell.py
@@ -95,36 +95,7 @@ for item in items:
     listbox.insert(tk.END, item)
 
 listbox.pack()
-<<<<<<< HEAD
-root.mainloop()
-
-# creates the frame with label for the text box
-frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
-frame_URL.pack()
-
-# decorative label
-url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
-    compound="center",
-    font=("comic sans", 14),
-    bd=0, 
-    relief=tk.FLAT, 
-    cursor="heart",
-    fg="mediumpurple3",
-    bg="black")
-url_label.pack(side=tk.LEFT)
-url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
-url_entry.pack(side=tk.LEFT)
-
-frame = tk.Frame(root,  bg="black") # change frame color
-frame.pack()
-
-# Adds an output box to GUI.
-command_textbox = tksc.ScrolledText(frame, height=10, width=100)
-command_textbox.pack()
-=======
  
 print(listbox.get())
    
-
 root.mainloop()
->>>>>>> 5666de8 (End)
